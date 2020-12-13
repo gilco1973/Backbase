a8a7ec74c7dbbe6acd618ef8a4a7a959f4ea1856
Gil Klainert <gil.klainert@intel.com>
2020-12-13 14:01:51 +0200

    amount validation

--- src/app/new-transfer/new-transfer.component.ts
+++ src/app/new-transfer/new-transfer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {CurrencyPipe} from "@angular/common";
 
@@ -11,8 +11,10 @@ export class NewTransferComponent implements OnInit {
   transferForm: FormGroup;
   availableBalance = 5824.76;
   arrowsUrl = 'assets/icons/arrows1.png';
+  amountValid: boolean;
 
-  constructor(private fb: FormBuilder, private currencyPipe: CurrencyPipe) { }
+  constructor(private fb: FormBuilder, private currencyPipe: CurrencyPipe) {
+  }
 
   ngOnInit(): void {
     this.transferForm = this.fb.group({
@@ -21,11 +23,20 @@ export class NewTransferComponent implements OnInit {
       amount: ['', Validators.required]
     });
     this.transferForm.valueChanges.subscribe(form => {
-      if(form.amount) {
+      this.amountValid = false;
+      if (form.amount) {
         form.amount = form.amount.replace('$', '').replace(',', '').replace('.', '');
-        this.transferForm.patchValue({
-          amount: this.currencyPipe.transform(form.amount, 'USD', 'symbol-narrow', '1.0-2')
-        }, {emitEvent: false})
+        if (!Number(form.amount)) {
+          this.transferForm.patchValue({amount: '$'}, {emitEvent: false});
+        } else if (form.amount > (this.availableBalance + 500)) {
+          this.transferForm.patchValue({amount: this.currencyPipe.transform((this.availableBalance + 500), 'USD', 'symbol-narrow', '1.0-2')}, {emitEvent: false});
+          this.amountValid = true;
+        } else {
+          this.transferForm.patchValue({
+            amount: this.currencyPipe.transform(form.amount, 'USD', 'symbol-narrow', '1.0-2')
+          }, {emitEvent: false});
+          this.amountValid = true;
+        }
       }
     });
   }
