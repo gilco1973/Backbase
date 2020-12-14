70b7176927a2a0772f9674ff2d45bd21c9d50f55
Gil Klainert <gil.klainert@intel.com>
2020-12-14 11:52:35 +0200

    amount validation

--- src/app/store/transfers-middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TransfersMiddleware } from './transfers-middleware';
-
-describe('TransfersMiddleware', () => {
-  it('should create an instance', () => {
-    expect(new TransfersMiddleware()).toBeTruthy();
-  });
-});
